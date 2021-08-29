8d78d1f1bafcfd0068602f3d9824a9911df94ac9
trodevel <trodevel@gmail.com>
2021-08-29 15:59:28 +0200

    adjsuted tool to new table format

--- add_search_keyword.sh
+++ add_search_keyword.sh
@@ -2,9 +2,9 @@
 
 #<hb>#############################################################################
 #
-# add_search_keyword.sh KEYWORD
+# add_search_keyword.sh CHAT_ID KEYWORD
 #
-# Example: ./add_search_keyword.sh react
+# Example: ./add_search_keyword.sh 12345 react
 #
 #<he>#############################################################################
 
@@ -14,14 +14,12 @@ sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -b3-    #"
 }
 
 
-KEYWORD=$1
-PASSW=$2
-SALUT=$3
-NAME=$4
-RIGHTS=$5
+CHAT_ID=$1
+KEYWORD=$2
 
+[[ -z "$CHAT_ID" ]] && echo "ERROR: need CHAT_ID" && print_help && exit 1
 [[ -z "$KEYWORD" ]] && echo "ERROR: need KEYWORD" && print_help && exit 1
 
-query=$( cat tmpl_add_search_keyword.sql | sed "s/%KEYWORD%/$KEYWORD/g" )
+query=$( cat tmpl_add_search_keyword.sql | sed "s/%CHAT_ID%/$CHAT_ID/g" | sed "s/%KEYWORD%/$KEYWORD/g" )
 
 echo $query | ./exec_query.sh
