2ea1ddb792182ab56de10701ab2994727301a31b
trodevel <trodevel@gmail.com>
2021-09-03 00:50:35 +0200

    SQL: added row number

--- tmpl_find_search_keywords_by_chat_id.sql
+++ tmpl_find_search_keywords_by_chat_id.sql
@@ -1,5 +1,11 @@
 USE xhunter;
 
-SELECT keyword
-FROM search_keywords
-WHERE chat_id = '%CHAT_ID%';
+SET @row_number = 0;
+
+SELECT
+    (@row_number:=@row_number + 1) AS num,
+    keyword
+FROM
+    search_keywords
+WHERE
+    chat_id = '%CHAT_ID%';
