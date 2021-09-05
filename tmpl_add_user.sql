838a687615a8d0c3790654aaebc553e2f46120d7
trodevel <trodevel@gmail.com>
2021-09-05 03:45:51 +0200

    SQL: bugfix: prevent error while inserting a duplicate user

--- tmpl_add_user.sql
+++ tmpl_add_user.sql
@@ -1,5 +1,5 @@
 USE xhunter;
-insert into users
+insert IGNORE into users
     ( id, username, firstname, lastname, is_bot, language_code )
 values
     ( '%ID%', '%USERNAME%', '%FIRSTNAME%', '%LASTNAME%', '%IS_BOT%', '%LANGUAGE_CODE%' );
