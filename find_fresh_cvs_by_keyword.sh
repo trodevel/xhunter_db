cd643a55cc088b8a0f5cabda36b214b48c2b49dd
trodevel <trodevel@gmail.com>
2021-09-06 01:09:20 +0200

    made age_mm parameterisaable

--- find_fresh_cvs_by_keyword.sh
+++ find_fresh_cvs_by_keyword.sh
@@ -2,9 +2,9 @@
 
 #<hb>#############################################################################
 #
-# find_fresh_cvs_by_keyword.sh KEYWORD
+# find_fresh_cvs_by_keyword.sh KEYWORD AGE_MM
 #
-# Example: ./find_fresh_cvs_by_keyword.sh react
+# Example: ./find_fresh_cvs_by_keyword.sh react 30
 #
 #<he>#############################################################################
 
@@ -14,9 +14,11 @@ sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -b3-    #"
 }
 
 KEYWORD=$1
+AGE_MM=$2
 
 [[ -z "$KEYWORD" ]] && echo "ERROR: need KEYWORD" && print_help && exit 1
+[[ -z "$AGE_MM" ]]  && echo "ERROR: need AGE_MM" && print_help && exit 1
 
-query=$( cat tmpl_find_fresh_cvs_by_keyword.sql | sed "s/%KEYWORD%/$KEYWORD/g" )
+query=$( cat tmpl_find_fresh_cvs_by_keyword.sql | sed "s/%KEYWORD%/$KEYWORD/g" | sed "s/%AGE_MM%/$AGE_MM/g" )
 
 echo "$query" | ./exec_query.sh
