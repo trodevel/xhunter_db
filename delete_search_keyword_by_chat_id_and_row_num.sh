aae37c5b4a20c259822ef54a777ccf97959385af
trodevel <trodevel@gmail.com>
2021-09-13 00:31:54 +0200

    added param ROW_NUM

--- delete_search_keyword_by_chat_id_and_row_num.sh
+++ delete_search_keyword_by_chat_id_and_row_num.sh
@@ -14,9 +14,11 @@ sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -b3-    #"
 }
 
 CHAT_ID=$1
+ROW_NUM=$2
 
 [[ -z "$CHAT_ID" ]] && echo "ERROR: need CHAT_ID" && print_help && exit 1
+[[ -z "$ROW_NUM" ]] && echo "ERROR: need ROW_NUM" && print_help && exit 1
 
-query=$( cat tmpl_delete_search_keyword_by_chat_id_and_row_num.sql | sed "s/%CHAT_ID%/$CHAT_ID/g" )
+query=$( cat tmpl_delete_search_keyword_by_chat_id_and_row_num.sql | sed "s/%CHAT_ID%/$CHAT_ID/g" | sed "s/%ROW_NUM%/$ROW_NUM/g" )
 
 echo "$query" | ./exec_query.sh
