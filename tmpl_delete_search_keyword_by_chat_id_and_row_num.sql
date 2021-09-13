ee70b86de0e73fd4ee66623d22b1ff6142ca1a53
trodevel <trodevel@gmail.com>
2021-09-14 01:04:14 +0200

    E6: add a query to delete a search request (#7)
    * added delete_search_keyword_by_chat_id_and_row_num
    
    * added param ROW_NUM
    
    * SQL: added delete_search_keyword_by_chat_id_and_row_num
    
    * SQL: put result into a variable
    
    * SQL: intermediate results
    
    * SQL: added deletion
    
    * SQL: added return values

---
+++ tmpl_delete_search_keyword_by_chat_id_and_row_num.sql
@@ -0,0 +1,32 @@
+USE xhunter;
+
+SET @row_number = 0;
+
+SET @keyword = (
+
+SELECT keyword
+FROM
+    (
+    SELECT
+        (@row_number:=@row_number + 1) AS num,
+        keyword
+    FROM
+        search_keywords
+    WHERE
+        chat_id = '%CHAT_ID%'
+    ) AS with_num
+WHERE
+        num = '%ROW_NUM%'
+);
+
+SELECT IF( @keyword IS NULL, 0, 1), '%ROW_NUM%', IF( @keyword IS NULL, '', @keyword );
+
+DELETE
+FROM
+    search_keywords
+WHERE
+    chat_id = '%CHAT_ID%'
+    AND
+    keyword = @keyword
+    AND
+    @keyword IS NOT NULL;
