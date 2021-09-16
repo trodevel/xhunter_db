0d26966df32be1e7987740e7dc8670d2debab446
trodevel <trodevel@gmail.com>
2021-09-16 16:15:48 +0200

    SQL: changed default

--- create_table_search_keywords.sql
+++ create_table_search_keywords.sql
@@ -5,6 +5,6 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 chat_id int (12) UNSIGNED NOT NULL,
 keyword varchar(255) CHARACTER SET utf8 NOT NULL,
 experience_from int (3) UNSIGNED NOT NULL DEFAULT 0,
-experience_to int (3) UNSIGNED NOT NULL DEFAULT 999,
+experience_to int (3) UNSIGNED NOT NULL DEFAULT 99,
 UNIQUE KEY uniq_rec ( chat_id, keyword, experience_from, experience_to )
 );
