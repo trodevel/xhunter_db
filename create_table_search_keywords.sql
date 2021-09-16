85888cb850e2edf8f77507b19bbfd2397f1195c2
trodevel <trodevel@gmail.com>
2021-09-16 12:47:11 +0200

    SQL: added defaults

--- create_table_search_keywords.sql
+++ create_table_search_keywords.sql
@@ -3,7 +3,7 @@ USE xhunter;
 CREATE TABLE search_keywords(
 chat_id int (12) UNSIGNED NOT NULL,
 keyword varchar(255) CHARACTER SET utf8 NOT NULL,
-experience_from int (3) UNSIGNED NOT NULL,
-experience_to int (3) UNSIGNED NOT NULL,
+experience_from int (3) UNSIGNED NOT NULL DEFAULT 0,
+experience_to int (3) UNSIGNED NOT NULL DEFAULT 999,
 PRIMARY KEY ( chat_id, keyword, experience_from, experience_to )
 );
