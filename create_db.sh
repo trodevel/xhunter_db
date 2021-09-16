bcc6fa4be4489c6287ae5ea84f05524192ef5823
trodevel <trodevel@gmail.com>
2021-09-16 16:16:23 +0200

    integrated create_table_map_chat_id_to_query_id

--- create_db.sh
+++ create_db.sh
@@ -5,6 +5,7 @@ echo "Creating new DB"
 scripts="create_db.sql \
 create_table_cvs.sql \
 create_table_cvs_temp.sql \
+create_table_map_chat_id_to_query_id.sql \
 create_table_map_keyword_to_cv.sql \
 create_table_search_keywords.sql \
 create_table_users.sql \
