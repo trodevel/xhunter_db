d07be5cfe0a6f282f6e7341f2a02c0801ede4ad4
trodevel <trodevel@gmail.com>
2021-09-16 16:21:37 +0200

    SQL: got rid of chat_id from search_keyword

--- create_table_search_keywords.sql
+++ create_table_search_keywords.sql
@@ -2,9 +2,8 @@ USE xhunter;
 
 CREATE TABLE IF NOT EXISTS search_keywords(
 id INT AUTO_INCREMENT PRIMARY KEY,
-chat_id int (12) UNSIGNED NOT NULL,
 keyword varchar(255) CHARACTER SET utf8 NOT NULL,
 experience_from int (3) UNSIGNED NOT NULL DEFAULT 0,
 experience_to int (3) UNSIGNED NOT NULL DEFAULT 99,
-UNIQUE KEY uniq_rec ( chat_id, keyword, experience_from, experience_to )
+UNIQUE KEY uniq_rec ( keyword, experience_from, experience_to )
 );
