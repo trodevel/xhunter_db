2a5aa8963604819dc071bbf093501c5ea45bc199
trodevel <trodevel@gmail.com>
2021-09-16 13:42:45 +0200

    SQL: added initialization of experience

--- tmpl_add_search_keyword.sql
+++ tmpl_add_search_keyword.sql
@@ -1,2 +1,3 @@
 USE xhunter;
-insert IGNORE into search_keywords ( chat_id, keyword ) values ( '%CHAT_ID%', '%KEYWORD%' );
+insert IGNORE into search_keywords ( chat_id, keyword, experience_from, experience_to )
+values ( '%CHAT_ID%', '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%' );
