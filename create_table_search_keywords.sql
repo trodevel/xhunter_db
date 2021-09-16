6043a96414967453cc7460b276a816318c5e6fda
trodevel <trodevel@gmail.com>
2021-09-16 14:12:09 +0200

    SQL: updated table format

--- create_table_search_keywords.sql
+++ create_table_search_keywords.sql
@@ -1,9 +1,10 @@
 USE xhunter;
 
 CREATE TABLE search_keywords(
+id INT AUTO_INCREMENT PRIMARY KEY,
 chat_id int (12) UNSIGNED NOT NULL,
 keyword varchar(255) CHARACTER SET utf8 NOT NULL,
 experience_from int (3) UNSIGNED NOT NULL DEFAULT 0,
 experience_to int (3) UNSIGNED NOT NULL DEFAULT 999,
-PRIMARY KEY ( chat_id, keyword, experience_from, experience_to )
+UNIQUE KEY uniq_rec ( chat_id, keyword, experience_from, experience_to )
 );
