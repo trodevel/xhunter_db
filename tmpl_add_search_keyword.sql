30aaba0326a5d1b832a59b2aad27f82cdecff478
trodevel <trodevel@gmail.com>
2021-09-16 17:19:46 +0200

    SQL: re-implemented tmpl_add_search_keyword

--- tmpl_add_search_keyword.sql
+++ tmpl_add_search_keyword.sql
@@ -1,3 +1,27 @@
 USE xhunter;
-insert IGNORE into search_keywords ( chat_id, keyword, experience_from, experience_to )
-values ( '%CHAT_ID%', '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%' );
+
+SET autocommit = OFF;
+
+START TRANSACTION;
+
+insert IGNORE into search_keywords ( keyword, experience_from, experience_to )
+values ( '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%' );
+
+SET @query_id =
+(
+    SELECT id
+    FROM search_keywords
+    WHERE
+        keyword = '%KEYWORD%'
+    AND
+        experience_from = '%EXPERIENCE_FROM%'
+    AND
+        experience_to = '%EXPERIENCE_TO%'
+);
+
+insert IGNORE into map_chat_id_to_query_id ( chat_id, query_id )
+values ( '%CHAT_ID%', @query_id );
+
+SELECT '%CHAT_ID%', @query_id;
+
+COMMIT;
