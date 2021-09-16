08f97402fd2bb23b5008ab45aedd4a91580497b6
trodevel <trodevel@gmail.com>
2021-09-16 14:11:02 +0200

    SQL: added index

--- attic/E8_add_columns.sql
+++ attic/E8_add_columns.sql
@@ -1,13 +1,16 @@
 USE xhunter;
 
 ALTER TABLE search_keywords
+DROP PRIMARY KEY;
+
+ALTER TABLE search_keywords
+ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
+
+ALTER TABLE search_keywords
 ADD COLUMN experience_from int (3) UNSIGNED NOT NULL DEFAULT 0;
 
 ALTER TABLE search_keywords
 ADD COLUMN experience_to int (3) UNSIGNED NOT NULL DEFAULT 999;
 
 ALTER TABLE search_keywords
-DROP PRIMARY KEY;
-
-ALTER TABLE search_keywords
-ADD PRIMARY KEY ( chat_id, keyword, experience_from, experience_to );
+ADD UNIQUE KEY uniq_rec( chat_id, keyword, experience_from, experience_to );
