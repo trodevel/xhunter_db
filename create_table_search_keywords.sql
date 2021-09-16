6d4f54c6c718e76e112db0ab03da44fc893bd396
trodevel <trodevel@gmail.com>
2021-09-16 12:44:22 +0200

    SQL: added new columns

--- create_table_search_keywords.sql
+++ create_table_search_keywords.sql
@@ -3,5 +3,7 @@ USE xhunter;
 CREATE TABLE search_keywords(
 chat_id int (12) UNSIGNED NOT NULL,
 keyword varchar(255) CHARACTER SET utf8 NOT NULL,
-PRIMARY KEY ( chat_id, keyword )
+experience_from int (3) UNSIGNED NOT NULL,
+experience_to int (3) UNSIGNED NOT NULL,
+PRIMARY KEY ( chat_id, keyword, experience_from, experience_to )
 );
