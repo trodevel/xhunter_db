8f3d14acdc2560432a77954031f802131d332f44
trodevel <trodevel@gmail.com>
2021-09-17 13:04:26 +0200

    SQL: added join

--- tmpl_find_search_queries_by_chat_id.sql
+++ tmpl_find_search_queries_by_chat_id.sql
@@ -1,10 +1,15 @@
 USE xhunter;
 
 SELECT
+    m.query_id,
     keyword,
     experience_from,
     experience_to
 FROM
-    search_queries
+    search_queries q
+JOIN
+    map_chat_id_to_query_id m
+ON
+    m.query_id = q.id
 WHERE
     chat_id = '%CHAT_ID%';
