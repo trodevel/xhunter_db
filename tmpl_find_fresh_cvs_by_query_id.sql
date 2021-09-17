a7fa6b871e6672a3928f7a489083986501b88222
trodevel <trodevel@gmail.com>
2021-09-17 23:26:11 +0200

    SQL: added p2, p3

--- tmpl_find_fresh_cvs_by_query_id.sql
+++ tmpl_find_fresh_cvs_by_query_id.sql
@@ -14,6 +14,26 @@ WHERE
     id = '%QUERY_ID%'
 );
 
+SET @p2=(
+
+SELECT
+    experience_from * 12
+FROM
+    search_queries
+WHERE
+    id = '%QUERY_ID%'
+);
+
+SET @p3=(
+
+SELECT
+    experience_to * 12
+FROM
+    search_queries
+WHERE
+    id = '%QUERY_ID%'
+);
+
 SELECT DISTINCT( c.id ), hash, subject, experience, modified_ts, age_mm
 FROM map_keyword_to_cv AS m
 JOIN
@@ -23,9 +43,11 @@ JOIN
 ) AS c
 ON m.id = c.id
 WHERE
-    m.keyword = @keyword
-AND
-    @keyword IS NOT NULL
+    m.keyword = @keyword AND @keyword IS NOT NULL
+    AND
+    c.experience >= @p2 AND @p2 IS NOT NULL
+    AND
+    c.experience <= @p3 AND @p3 IS NOT NULL
 AND
     age_mm <= '%AGE_MM%';
 
