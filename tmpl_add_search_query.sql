2a423bfac47cd01b9ed3c3e08b08452b41ceb9c7
trodevel <trodevel@gmail.com>
2021-09-18 01:12:16 +0200

    SQL: add a check whether the values are already present in the table

--- tmpl_add_search_query.sql
+++ tmpl_add_search_query.sql
@@ -4,8 +4,25 @@ SET autocommit = OFF;
 
 START TRANSACTION;
 
-insert IGNORE into search_queries ( keyword, experience_from, experience_to )
-values ( '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%' );
+SET @query_id =
+(
+    SELECT id
+    FROM search_queries
+    WHERE
+        keyword = '%KEYWORD%'
+    AND
+        experience_from = '%EXPERIENCE_FROM%'
+    AND
+        experience_to = '%EXPERIENCE_TO%'
+);
+
+insert into search_queries ( keyword, experience_from, experience_to )
+SELECT
+     '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%'
+FROM
+    DUAL
+WHERE
+    @query_id IS NOT NULL;
 
 SET @query_id =
 (
