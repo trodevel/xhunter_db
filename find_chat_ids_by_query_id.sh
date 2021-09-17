8b85164e12a2d8c338ea64a6ce9dcba66e21a6da
trodevel <trodevel@gmail.com>
2021-09-17 13:14:34 +0200

    migrated from KEYWORD to QUERY_ID

--- find_chat_ids_by_query_id.sh
+++ find_chat_ids_by_query_id.sh
@@ -2,9 +2,9 @@
 
 #<hb>#############################################################################
 #
-# find_chat_ids_by_keyword.sh KEYWORD
+# find_chat_ids_by_query_id.sh QUERY_ID
 #
-# Example: ./find_chat_ids_by_keyword.sh react
+# Example: ./find_chat_ids_by_query_id.sh react
 #
 #<he>#############################################################################
 
@@ -13,10 +13,10 @@ print_help()
 sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -b3-    #"
 }
 
-KEYWORD=$1
+QUERY_ID=$1
 
-[[ -z "$KEYWORD" ]] && echo "ERROR: need KEYWORD" && print_help && exit 1
+[[ -z "$QUERY_ID" ]] && echo "ERROR: need QUERY_ID" && print_help && exit 1
 
-query=$( cat tmpl_find_chat_ids_by_keyword.sql | sed "s/%KEYWORD%/$KEYWORD/g" )
+query=$( cat tmpl_find_chat_ids_by_query_id.sql | sed "s/%QUERY_ID%/$QUERY_ID/g" )
 
 echo "$query" | ./exec_query.sh
