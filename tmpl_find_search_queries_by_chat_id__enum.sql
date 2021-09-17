30e3e23ff9935d1eb5442ede3dbf9d0a4c53017a
trodevel <trodevel@gmail.com>
2021-09-17 13:12:22 +0200

    SQL: added join

--- tmpl_find_search_queries_by_chat_id__enum.sql
+++ tmpl_find_search_queries_by_chat_id__enum.sql
@@ -4,10 +4,15 @@ SET @row_number = 0;
 
 SELECT
     (@row_number:=@row_number + 1) AS num,
+    m.query_id,
     keyword,
     experience_from,
     experience_to
 FROM
-    search_queries
+    search_queries q
+JOIN
+    map_chat_id_to_query_id m
+ON
+    m.query_id = q.id
 WHERE
     chat_id = '%CHAT_ID%';
