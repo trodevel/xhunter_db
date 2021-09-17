08f6e995c68ba0f6017c535b96f9207443a37b87
trodevel <trodevel@gmail.com>
2021-09-18 01:34:08 +0200

    B13: crash upon insertion of a duplicate query (#15)
    * SQL: add a check whether the values are already present in the table
    
    * bugfix
    
    * SQL: added num_mappings
    
    * SQL: re-implemented insertion into map_chat_id_to_query_id

--- tmpl_add_search_query.sql
+++ tmpl_add_search_query.sql
@@ -4,9 +4,6 @@ SET autocommit = OFF;
 
 START TRANSACTION;
 
-insert IGNORE into search_queries ( keyword, experience_from, experience_to )
-values ( '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%' );
-
 SET @query_id =
 (
     SELECT id
@@ -19,9 +16,46 @@ SET @query_id =
         experience_to = '%EXPERIENCE_TO%'
 );
 
-insert IGNORE into map_chat_id_to_query_id ( chat_id, query_id )
-values ( '%CHAT_ID%', @query_id );
+insert into search_queries ( keyword, experience_from, experience_to )
+SELECT
+     '%KEYWORD%', '%EXPERIENCE_FROM%', '%EXPERIENCE_TO%'
+FROM
+    DUAL
+WHERE
+    @query_id IS NULL;
 
-SELECT '%CHAT_ID%', @query_id;
+SET @new_query_id =
+(
+    SELECT id
+    FROM search_queries
+    WHERE
+        keyword = '%KEYWORD%'
+    AND
+        experience_from = '%EXPERIENCE_FROM%'
+    AND
+        experience_to = '%EXPERIENCE_TO%'
+);
+
+SET @num_mappings =
+(
+SELECT
+    COUNT(*)
+FROM
+    map_chat_id_to_query_id
+WHERE
+    chat_id = '%CHAT_ID%'
+    AND
+    query_id = @new_query_id
+);
+
+insert into map_chat_id_to_query_id ( chat_id, query_id )
+SELECT
+    '%CHAT_ID%', @new_query_id
+FROM
+    DUAL
+WHERE
+    @num_mappings = 0;
+
+SELECT '%CHAT_ID%', @new_query_id;
 
 COMMIT;
