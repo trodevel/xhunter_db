7124ff28428df870d711527bc98cef187919dc4e
trodevel <trodevel@gmail.com>
2021-10-23 02:28:47 +0200

    extracted incl_import_from_temp.sql from template

---
+++ incl_import_from_temp.sql
@@ -0,0 +1,71 @@
+# should be included from tmpl_import_cvs_....sql
+
+# DEBUG
+#SELECT 'DEBUG:', id, source_id, foreign_id, recv_ts, modified_ts, subject, experience FROM cvs_temp;
+
+SELECT COUNT(*) FROM cvs_temp t1
+WHERE NOT EXISTS
+(
+      SELECT 1
+      FROM cvs t2 WHERE
+      t2.foreign_id = t1.foreign_id
+      AND
+      t2.source_id = t1.source_id
+);
+
+# DEBUG
+#SELECT 'DEBUG: current size of tmp table:', COUNT(*) FROM cvs_temp t1;
+
+# DEBUG
+#SELECT 'DEBUG: existing records:', COUNT(*)
+#FROM cvs_temp t2
+#JOIN cvs t1
+#ON
+#    t2.foreign_id = t1.foreign_id
+#    AND
+#    t2.source_id = t1.source_id;
+
+# DEBUG
+#SELECT 'DEBUG: id', t2.id
+#FROM cvs_temp t2
+#JOIN cvs t1
+#ON
+#    t2.foreign_id = t1.foreign_id
+#    AND
+#    t2.source_id = t1.source_id;
+
+# delete records from the temp table which already present in the main table
+DELETE FROM cvs_temp
+WHERE id
+IN
+(
+    SELECT *
+    FROM
+    (
+        SELECT t2.id
+        FROM cvs_temp t2
+        JOIN cvs t1
+        ON
+            t2.foreign_id = t1.foreign_id
+            AND
+            t2.source_id = t1.source_id
+    ) AS p
+);
+
+# DEBUG
+#SELECT 'DEBUG: new size of tmp table', COUNT(*) FROM cvs_temp t1;
+
+INSERT INTO cvs SELECT NULL, source_id, foreign_id, recv_ts, modified_ts, subject, experience FROM cvs_temp t1;
+
+INSERT INTO map_keyword_to_cv ( keyword, id )
+SELECT @KEYWORD, id
+FROM
+(
+    SELECT t1.id
+    FROM cvs_temp t2
+    JOIN cvs t1
+    ON
+        t2.foreign_id = t1.foreign_id
+        AND
+        t2.source_id = t1.source_id
+) AS p;
