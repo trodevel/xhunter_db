acf973c3d89086791efbd3e8abcf2d740b8f0972
trodevel <trodevel@gmail.com>
2021-10-23 02:22:41 +0200

    file renaming

--- import_cvs__hh_ru.sh
+++ import_cvs__hh_ru.sh
@@ -2,9 +2,9 @@
 
 #<hb>#############################################################################
 #
-# import_cvs.sh FILENAME KEYWORD
+# import_cvs__hh_ru.sh FILENAME KEYWORD
 #
-# Example: ./import_cvs.sh new_cv.csv java
+# Example: ./import_cvs__hh_ru.sh new_cv.csv java
 #
 #<he>#############################################################################
 
@@ -20,7 +20,7 @@ KEYWORD=$2
 [[ -z "$FILENAME" ]] && echo "ERROR: need FILENAME" && print_help && exit 1
 [[ -z "$KEYWORD" ]] && echo "ERROR: need KEYWORD" && print_help && exit 1
 
-query=$( cat tmpl_import_cvs.sql | sed "s~%FILENAME%~$FILENAME~g" | sed "s/%KEYWORD%/$KEYWORD/g" )
+query=$( cat tmpl_import_cvs__hh_ru.sql | sed "s~%FILENAME%~$FILENAME~g" | sed "s/%KEYWORD%/$KEYWORD/g" )
 
 set -o noglob
 echo "$query" | ./exec_query.sh
