737d29ad84a04069de7192ec8c850652530b07f9
trodevel <trodevel@gmail.com>
2021-10-30 00:09:18 +0200

    SQL: added unique key for map_keyword_to_cv

--- create_table_map_keyword_to_cv.sql
+++ create_table_map_keyword_to_cv.sql
@@ -2,5 +2,6 @@ USE xhunter;
 
 CREATE TABLE IF NOT EXISTS map_keyword_to_cv(
 keyword varchar(255) CHARACTER SET utf8 NOT NULL,
-id int (11) UNSIGNED NOT NULL
+id int (11) UNSIGNED NOT NULL,
+UNIQUE KEY uniq_rec ( keyword, id )
 );
