ab6fe3c5f1f67b0f60b9d803b84ba30f52323417
trodevel <trodevel@gmail.com>
2021-10-30 02:40:26 +0200

    SQL: activated insertion of new mappings

--- incl_import_from_temp.sql
+++ incl_import_from_temp.sql
@@ -109,19 +109,33 @@ SET @new_mappings =
 SELECT @num_cvs, @existing_cvs, @new_cvs, @existing_mappings, @new_mappings;
 
 # update keyword-to-cv mapping for existing CVs
-#INSERT INTO map_keyword_to_cv ( keyword, id )
-#SELECT @KEYWORD, id
-#FROM
-#(
-#    SELECT t1.id
-#    FROM cvs_temp t2
-#    JOIN cvs t1
-#    ON
-#        t2.foreign_id = t1.foreign_id
-#        AND
-#        t2.source_id = t1.source_id
-#) AS p;
-
+INSERT INTO map_keyword_to_cv ( keyword, id )
+FROM
+(
+    SELECT keyword, id
+    FROM
+    (
+        SELECT @KEYWORD keyword, p.id
+        FROM
+        (
+            SELECT t1.id
+            FROM cvs_temp t2
+            JOIN cvs t1
+            ON
+                t2.foreign_id = t1.foreign_id
+                AND
+                t2.source_id = t1.source_id
+        ) AS p
+    ) AS em
+    WHERE ( keyword, id )
+        NOT IN
+        (
+        SELECT keyword, id
+        FROM map_keyword_to_cv
+        WHERE
+            keyword = @KEYWORD
+        )
+);
 
 # delete records from the temp table which already present in the main table
 DELETE FROM cvs_temp
