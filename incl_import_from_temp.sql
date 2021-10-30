95e43a29a426234c2533a8f3ebd461b27e1cfab0
trodevel <trodevel@gmail.com>
2021-10-30 02:38:04 +0200

    SQL: added counting of new mappings

--- incl_import_from_temp.sql
+++ incl_import_from_temp.sql
@@ -78,21 +78,49 @@ SET @existing_mappings =
         @KEYWORD = m.keyword
 );
 
-SELECT @num_cvs, @existing_cvs, @new_cvs, @existing_mappings;
+SET @new_mappings =
+(
+#   SELECT keyword, id
+    SELECT COUNT(*)
+    FROM
+    (
+        SELECT @KEYWORD keyword, p.id
+        FROM
+        (
+            SELECT t1.id
+            FROM cvs_temp t2
+            JOIN cvs t1
+            ON
+                t2.foreign_id = t1.foreign_id
+                AND
+                t2.source_id = t1.source_id
+        ) AS p
+    ) AS em
+    WHERE ( keyword, id )
+        NOT IN
+        (
+        SELECT keyword, id
+        FROM map_keyword_to_cv
+        WHERE
+            keyword = @KEYWORD
+        )
+);
+
+SELECT @num_cvs, @existing_cvs, @new_cvs, @existing_mappings, @new_mappings;
 
 # update keyword-to-cv mapping for existing CVs
-INSERT INTO map_keyword_to_cv ( keyword, id )
-SELECT @KEYWORD, id
-FROM
-(
-    SELECT t1.id
-    FROM cvs_temp t2
-    JOIN cvs t1
-    ON
-        t2.foreign_id = t1.foreign_id
-        AND
-        t2.source_id = t1.source_id
-) AS p;
+#INSERT INTO map_keyword_to_cv ( keyword, id )
+#SELECT @KEYWORD, id
+#FROM
+#(
+#    SELECT t1.id
+#    FROM cvs_temp t2
+#    JOIN cvs t1
+#    ON
+#        t2.foreign_id = t1.foreign_id
+#        AND
+#        t2.source_id = t1.source_id
+#) AS p;
 
 
 # delete records from the temp table which already present in the main table
