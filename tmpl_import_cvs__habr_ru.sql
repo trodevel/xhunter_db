f13ee5c05ed9b1b704db1c0e762f2af46123ba3a
trodevel <trodevel@gmail.com>
2021-11-04 00:47:06 +0100

    updated importer habr_ru

--- tmpl_import_cvs__habr_ru.sql
+++ tmpl_import_cvs__habr_ru.sql
@@ -17,7 +17,7 @@ CHARACTER SET UTF8
 FIELDS TERMINATED BY ';'
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-( foreign_id, @dummy, @epoch, subject, @dummy2, @dummy3, @dummy4, @dummy5, experience )
+( foreign_id, @dummy, @epoch, subject, @dummy2, @dummy3, @dummy4, @dummy5, experience, @dummy6, last_position, last_company, last_experience )
 set modified_ts = FROM_UNIXTIME( @epoch ), source_id = 2;
 
 SOURCE incl_import_from_temp.sql
